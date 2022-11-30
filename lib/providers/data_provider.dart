1e0d841a49509707c84a493812931c85935cb629
jguegon966 <jguegon966@g.educaand.es>
2022-11-30 09:32:46 +0100

    iseneca12

--- lib/providers/data_provider.dart
+++ lib/providers/data_provider.dart
@@ -7,27 +7,18 @@ import 'package:http/http.dart' as http;
 
 import 'package:flutter/material.dart';
 
-class ProveedorDatos extends ChangeNotifier{
-
+class ProveedorDatos extends ChangeNotifier {
   Future<List<User>> getDataFromGoogleSheet() async {
-
-    Response data = await http.get(Uri.parse('https://script.google.com/macros/s/AKfycbzq6EP_1jV1RP7YO7dVMl0wRApuSWrQWWcOMdpbL6okGek_y3anbmIqQU48iOBz3h9m/exec?spreadsheetId=1SPFk04l6FtyrZlWDTGGSsMZhXuLTrPrGUuLB3BUCvcs&sheet=usuarios'));
+    var url =
+        'https://script.google.com/macros/s/AKfycbzq6EP_1jV1RP7YO7dVMl0wRApuSWrQWWcOMdpbL6okGek_y3anbmIqQU48iOBz3h9m/exec?spreadsheetId=1SPFk04l6FtyrZlWDTGGSsMZhXuLTrPrGUuLB3BUCvcs&sheet=usuarios';
+    Response data = await http.get(Uri.parse(url));
     dynamic jsonAppData = convert.jsonDecode(data.body);
-    final List<User> listaUsuarios =[];
-    for(dynamic data in jsonAppData){
-
-      User user = User(
-        usuario: data['usuario'], 
-        clave: data['clave']
-      );
+    final List<User> listaUsuarios = [];
+    for (dynamic data in jsonAppData) {
+      User user = User(usuario: data['usuario'], clave: data['clave']);
       listaUsuarios.add(user);
     }
 
     return listaUsuarios;
-
   }
-  
 }
-
-
-
